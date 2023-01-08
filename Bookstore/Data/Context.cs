b69a206fec59f62626caa44173cf832b26e50fdb
Aaron Fraga <aronfraga@gmail.com>
2023-01-08 12:10:20 -0300

    first commit

---
+++ Bookstore/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Bookstore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Data {
+    public class Context : DbContext {
+
+        private readonly DbContext _context;
+
+        public Context(DbContextOptions<Context> options) : base(options) {
+            _context = context; 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Bookstore/Models/Category.cs
@@ -0,0 +1,4 @@
+﻿namespace Bookstore.Models {
+    public class Category {
+    }
+}
