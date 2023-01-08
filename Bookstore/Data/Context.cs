90843d82c5ad214b3a55b2fbcfddd3789debf9c6
Aaron Fraga <aronfraga@gmail.com>
2023-01-08 12:20:22 -0300

    working on models and data

--- Bookstore/Data/Context.cs
+++ Bookstore/Data/Context.cs
@@ -4,10 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Bookstore.Data {
     public class Context : DbContext {
 
-        private readonly DbContext _context;
-
         public Context(DbContextOptions<Context> options) : base(options) {
-            _context = context; 
         }
 
         public DbSet<Category> Categories { get; set; }
--- Bookstore/Program.cs
+++ Bookstore/Program.cs
@@ -1,7 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Bookstore.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var ConnectionString = builder.Configuration.GetConnectionString("dbConnection");
+
+builder.Services.AddDbContext<Context>(data => data.UseSqlServer(ConnectionString));
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment()) {
